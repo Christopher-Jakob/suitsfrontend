d6e70b696e962b851a929e42edeb06641e5e9b26
Christopher Jakob <christopher.m.jakob@gmail.com>
2018-09-29 10:38:13 -0500

    pushing

--- src/app/mainroot/landingpage/landingpage.component.ts
+++ src/app/mainroot/landingpage/landingpage.component.ts
@@ -15,12 +15,13 @@ import {VenueService} from "../../services/venueservice/venueservice";
 })
 export class LandingpageComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router, private route: ActivatedRoute, private landingpageservice: LandingpageDependancyService, private navcomm : BrowsevenuescomponentcommService, private venueservice: VenueService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private landingpageservice: LandingpageDependancyService, private navcomm: BrowsevenuescomponentcommService, private venueservice: VenueService) { }
 
   isLeftVisible = true;
   choicesload = true;
 
   @ViewChild('selectform') selectform : NgForm;
+  @ViewChild('selectformlarge') selectformlarge : NgForm;
   loaded = false;
   selectedcity;
   cities =[
@@ -30,6 +31,46 @@ export class LandingpageComponent implements OnInit, OnDestroy {
 
   browse(){
     this.selectedcity = this.selectform.value.cityselect;
+    this.navcomm.setselectedcity(this.selectedcity);
+    let citypk = null;
+    for(let city in this.cities){
+      if(this.cities[city].city === this.selectedcity){
+        citypk = this.cities[city].pk;
+      }
+    }
+    this.navcomm.sendselectedcity(citypk);
+    this.router.navigate(['/browse', this.selectedcity]);
+
+  }
+  signupcompelete = false;
+  createvenue(form:NgForm){
+    let payload = {
+      name: form.value.venuename,
+      streetaddress1: form.value.streetaddress1,
+      streetaddress2: form.value.streetaddress2,
+      city: form.value.city,
+      state: form.value.venuestateselect,
+      venuephone: form.value.phone,
+      venuecontactname: form.value.contactname,
+      venuecontactjobtitle: form.value.jobtitle,
+      venuecontactemail: form.value.email,
+      phonenumber: form.value.phone
+    };
+
+    if(form.valid){
+      this.venueservice.submitapplication(payload)
+        .subscribe(
+          (req: any)=>{
+            this.signupcompelete = true;
+          }
+        );
+
+
+    }
+  }
+
+  browselarge(){
+    this.selectedcity = this.selectformlarge.value.cityselect;
     this.navcomm.setselectedcity(this.selectedcity);
     let citypk = null;
     for(let city in this.cities){
